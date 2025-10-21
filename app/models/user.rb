e8cae33c41421aa1876fe79ffbf3f9b1edcb143f
mionpuuu <mionikeya@gmail.com>
2025-10-21 12:35:48 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true
+
+  # has_many :events
+  # has_many :attendances
+  # has_many :notices
 end
