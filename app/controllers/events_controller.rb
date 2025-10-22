c35ee2dcfa97e028adcbc35fd1f6b12a74e2eea0
mionpuuu <mionikeya@gmail.com>
2025-10-22 10:59:22 +0900

    ダッシュボード作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,28 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.order(date: :asc)
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to events_path, notice: 'イベントを作成しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date, :time, :place, :memo, :payment, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'dashboards/index'
   devise_for :users
-  root to: "events#index"
+  root to: "dashboards#index"
+  resources :events
+  resources :notices
 end
