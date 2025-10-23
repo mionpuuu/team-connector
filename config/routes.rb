968c9a5067dffe8b2154619c6dd60f65db50136d
mionpuuu <mionikeya@gmail.com>
2025-10-23 18:01:23 +0900

    編集削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "dashboards#index"
-  resources :events, only:[:index, :new, :create, :show]
+  resources :events
   resources :notices, only: [:index,:new, :create, :show]
 end
