495ca717e6dc02f54787142d69b1daba304ae16a
mionpuuu <mionikeya@gmail.com>
2025-10-23 14:23:58 +0900

    アイコンを変更

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,11 @@
 class DashboardsController < ApplicationController
   def index
-    @events = Event.all
+    # 今日以降の試合を日付の近い順に5件だけ表示
+    @events = Event.where("date >= ?", Date.today)
+                   .order(date: :asc)
+                   .limit(5)
+
+    # お知らせは最新順で5件
     @notices = Notice.order(created_at: :desc).limit(5)
   end
 end
