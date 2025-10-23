8285ddfa44b368b289cf3fa0809b5ff88cd7e382
mionpuuu <mionikeya@gmail.com>
2025-10-23 16:53:56 +0900

    ビューの更新

---
+++ config/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './app/views/**/*.{erb,html}',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
