149a5b1b07ceb96c3e3525b07ab408f2e9a1d2c0
mionpuuu <mionikeya@gmail.com>
2025-10-28 11:56:23 +0900

    attendanceモデルのテストコード

--- spec/factories/attendances.rb
+++ spec/factories/attendances.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :attendance do
-    
+    association :user
+    association :event
+    status { :attending }
   end
 end
--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @event = FactoryBot.create(:event, user: @user)
+    @attendance = FactoryBot.build(:attendance, user: @user, event: @event)
+  end
+
+  describe '出欠登録' do
+    context '登録できるとき' do
+      it 'userとeventが紐づいていれば登録できる' do
+        expect(@attendance).to be_valid
+      end
+    end
+
+    context '登録できないとき' do
+      it 'userがなければ登録できない' do
+        @attendance.user = nil
+        @attendance.valid?
+        expect(@attendance.errors.full_messages).to include('User must exist')
+      end
+
+      it 'eventがなければ登録できない' do
+        @attendance.event = nil
+        @attendance.valid?
+        expect(@attendance.errors.full_messages).to include('Event must exist')
+      end
+    end
+  end
+
+  describe 'enumの動作' do
+    it 'statusの値が正しく定義されていること' do
+      expect(Attendance.statuses).to eq({
+        'undecided' => 0,
+        'attending' => 1,
+        'absent' => 2,
+        'pending' => 3
+      })
+    end
+  end
 end
