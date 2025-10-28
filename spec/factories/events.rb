d73ac71fedb719634221e8af9bdfafd073e2cbe9
mionpuuu <mionikeya@gmail.com>
2025-10-28 11:48:36 +0900

    Eventsモデルのテストコード

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :event do
-    
+    association :user
+    title { "練習試合" }
+    date { Date.today + 7 }
+    location { "渋谷体育館" }
+    description { "渋谷体育館で練習試合を行います。" }
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @event = FactoryBot.build(:event, user: @user)
+  end
+
+  describe 'イベント新規登録' do
+    context '登録できるとき' do
+      it '全ての項目が存在すれば登録できる' do
+        expect(@event).to be_valid
+      end
+    end
+
+    context '登録できないとき' do
+      it 'タイトルが空では登録できない' do
+        @event.title = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it '日付が空では登録できない' do
+        @event.date = nil
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Date can't be blank")
+      end
+
+      it '場所が空では登録できない' do
+        @event.location = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Location can't be blank")
+      end
+
+      it '詳細が空では登録できない' do
+        @event.description = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'ユーザーが紐づいていないと登録できない' do
+        @event.user = nil
+        @event.valid?
+        expect(@event.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
